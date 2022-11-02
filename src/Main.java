79359a76cfa212d307f4cce249678a4812bf4d4d
BT_3W2_04 <vincentl105@nycstudents.net>
2022-11-02 08:46:22 -0400

    2nd do now

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int num = myScanner.nextInt();
+        System.out.print("Enter a lowercase word: ");
+        String word = myScanner.nextLine();
+        String first = word.substring(0, 1);  // get first letter
 
-        if ((num >= -50) && (num <= 50)) {
-            System.out.println("Your number is between -50 and 50");
-        } else if ((num < -100) || (num > 100)) {
-            System.out.println("Your number is more then 100 away from 0!");
+        if ((first.equals ("a")) || (first.equals ("e")) || (first.equals ("i")) || (first.equals ("o")) || (first.equals ("u"))) {
+            System.out.println("Your word begins with a vowel");
         } else {
-            System.out.println("Something else");
+            System.out.println("Your word begins with a consonant");
         }
 
         myScanner.close();
